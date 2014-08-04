88ef028745e71462508f3bae6f4fe79730e00ebf
Ioan Loosley <ioanthecomputerguy@gmail.com>
2014-08-04 16:23:43 +0100

    added files
    f

---
+++ desktop/Recever/barcode lissen/barcode lissen/Form1.Designer.vb
@@ -0,0 +1,116 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Settingsbutton = New System.Windows.Forms.Button()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SplitContainer1.Panel1.SuspendLayout()
+        Me.SplitContainer1.Panel2.SuspendLayout()
+        Me.SplitContainer1.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'SplitContainer1
+        '
+        Me.SplitContainer1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SplitContainer1.Location = New System.Drawing.Point(0, 0)
+        Me.SplitContainer1.Name = "SplitContainer1"
+        Me.SplitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal
+        '
+        'SplitContainer1.Panel1
+        '
+        Me.SplitContainer1.Panel1.Controls.Add(Me.GroupBox1)
+        '
+        'SplitContainer1.Panel2
+        '
+        Me.SplitContainer1.Panel2.Controls.Add(Me.Settingsbutton)
+        Me.SplitContainer1.Size = New System.Drawing.Size(576, 388)
+        Me.SplitContainer1.SplitterDistance = 326
+        Me.SplitContainer1.TabIndex = 0
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.GroupBox1.Location = New System.Drawing.Point(0, 0)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(576, 326)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Log"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TextBox1.Location = New System.Drawing.Point(3, 16)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBox1.Size = New System.Drawing.Size(570, 307)
+        Me.TextBox1.TabIndex = 0
+        Me.TextBox1.WordWrap = False
+        '
+        'Settingsbutton
+        '
+        Me.Settingsbutton.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Settingsbutton.Location = New System.Drawing.Point(0, 0)
+        Me.Settingsbutton.Name = "Settingsbutton"
+        Me.Settingsbutton.Size = New System.Drawing.Size(576, 58)
+        Me.Settingsbutton.TabIndex = 0
+        Me.Settingsbutton.Text = "Settings"
+        Me.Settingsbutton.UseVisualStyleBackColor = True
+        '
+        'BackgroundWorker1
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(576, 388)
+        Me.Controls.Add(Me.SplitContainer1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Barcode Scanner Recerver"
+        Me.SplitContainer1.Panel1.ResumeLayout(False)
+        Me.SplitContainer1.Panel2.ResumeLayout(False)
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SplitContainer1.ResumeLayout(False)
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents SplitContainer1 As System.Windows.Forms.SplitContainer
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Settingsbutton As System.Windows.Forms.Button
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+
+End Class
---
+++ desktop/Recever/barcode lissen/barcode lissen/Form1.vb
@@ -0,0 +1,69 @@
+﻿Imports System
+Imports System.Net
+Imports System.Net.Sockets
+Imports System.Text
+
+Public Class Form1
+
+    ' The delegate
+    Delegate Sub AppendTextbox_Delegate(ByVal [textbox] As TextBox, ByVal [text] As String)
+
+    ' The delegates subroutine.
+    Private Sub AppendTextbox_ThreadSafe(ByVal [textbox] As TextBox, ByVal [text] As String)
+        ' InvokeRequired required compares the thread ID of the calling thread to the thread ID of the creating thread.
+        ' If these threads are different, it returns true.
+        If [textbox].InvokeRequired Then
+            Dim MyDelegate As New AppendTextbox_Delegate(AddressOf AppendTextbox_ThreadSafe)
+            Me.Invoke(MyDelegate, New Object() {[textbox], [text]})
+        Else
+            [textbox].AppendText([text])
+        End If
+    End Sub
+
+
+    Private Sub log(ByRef text As String, ByRef iserror As Boolean)
+        If iserror = True Then
+            AppendTextbox_ThreadSafe(TextBox1, "[Warn] " + text)
+        Else
+            AppendTextbox_ThreadSafe(TextBox1, "[Info] " + text)
+        End If
+        AppendTextbox_ThreadSafe(TextBox1, Environment.NewLine)
+    End Sub
+
+    Private Sub emulatekeys(ByRef text As String)
+        SendKeys.SendWait(text)
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        BackgroundWorker1.RunWorkerAsync()
+    End Sub
+    Private Const listenPort As Integer = 11000
+
+    Private Sub StartListener()
+        Dim done As Boolean = False
+        Dim listener As New UdpClient(listenPort)
+        Dim groupEP As New IPEndPoint(IPAddress.Any, listenPort)
+        Try
+            While Not done
+                log("Waiting for barcode scanner", False)
+                Dim bytes As Byte() = listener.Receive(groupEP)
+                log("Received broadcast from {0} :" + groupEP.ToString(), False)
+                Dim msg As String = Encoding.ASCII.GetString(bytes, 0, bytes.Length)
+                log("Barcode contents are: " + msg, True)
+                emulatekeys(msg)
+                emulatekeys("{ENTER}")
+            End While
+        Catch e As Exception
+            log(e.ToString(), True)
+        Finally
+            listener.Close()
+        End Try
+    End Sub 'StartListener
+    Private Sub Settingsbutton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Settingsbutton.Click
+
+    End Sub
+
+    Private Sub BackgroundWorker1_DoWork(ByVal sender As System.Object, ByVal e As System.ComponentModel.DoWorkEventArgs) Handles BackgroundWorker1.DoWork
+        StartListener()
+    End Sub
+End Class
